04a350966d1322e29e82dabfee1c3d7bf39c8ffd
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-10-13 13:03:11 -0500

    import skeleton project

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Joe Vargas on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
