5ee99f5ef46ab888660eea6f1178720c130f89dd
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-10-13 13:07:24 -0500

    Folder structure set up

---
+++ UberSwiftUI/Core/Views/HomeVIew.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVIew.swift
+//  UberSwiftUI
+//
+//  Created by Joe Vargas on 10/13/22.
+//
+
+import SwiftUI
+
+struct HomeVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeVIew()
+    }
+}
