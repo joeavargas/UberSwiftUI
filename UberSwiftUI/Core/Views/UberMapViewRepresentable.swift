ec3a73d7bce6ead75f37bd655b35956dd63c147c
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-10-13 13:35:37 -0500

    Show user's location on the mapview

---
+++ UberSwiftUI/Core/Views/UberMapViewRepresentable.swift
@@ -0,0 +1,51 @@
+//
+//  UberMapViewRepresentable.swift
+//  UberSwiftUI
+//
+//  Created by Joe Vargas on 10/13/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    let locationManager = LocationManager()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordiantor {
+        return MapCoordiantor(parent: self)
+    }
+}
+
+extension UberMapViewRepresentable {
+    class MapCoordiantor: NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+        
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            
+            parent.mapView.setRegion(region, animated: true)
+        }
+    }
+}
