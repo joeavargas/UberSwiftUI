7b91eaccbb82321a4465ffbe614e3a63dc5b2178
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-10-20 18:00:07 -0500

    Add dark mode support

---
+++ UberSwiftUI/Extensions/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  UberSwiftUI
+//
+//  Created by Joe Vargas on 10/20/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let backgroundColor = Color("BackgroundColor")
+    let secondaryBackgroundColor = Color("SecondaryBackgroundColor")
+    let primaryTextColor = Color("PrimaryTextColor")
+}
