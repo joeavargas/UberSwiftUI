d3934544c0215ece89a5be3bcbbcd46d90e450f7
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-10-21 22:46:20 -0500

    Added firebase dependencies

--- UberSwiftUI/UberSwiftUIApp.swift
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct UberSwiftUIApp: App {
     @StateObject var locationSearchVM = LocationSearchViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
 //            HomeView()
